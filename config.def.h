c62353d636807202bdf1fecc4800a104bcfc7be6
Derek Taylor <derek@distrotube.com>
2021-03-08 15:08:45 -0600

    Patched for keychords.

--- config.def.h
+++ config.def.h
@@ -29,14 +29,15 @@ static const char col_4[]  = "#924441"; /* border color focused windows and tags
 /* bar opacity 
  * 0xff is no transparency.
  * 0xee adds wee bit of transparency.
+ * 0xdd adds adds a bit more transparency.
  * Play with the value to get desired transparency.
  */
-static const unsigned int baralpha    = 0xee; 
+static const unsigned int baralpha    = 0xff; 
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]        = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_3, col_1, col_2 },
-	[SchemeSel]  = { col_3, col_4,  col_4 },
+	[SchemeSel]  = { col_3, col_4, col_4 },
 };
 static const unsigned int alphas[][3] = {
 	/*               fg      bg        border     */
@@ -47,7 +48,6 @@ static const unsigned int alphas[][3] = {
 /* tagging */
 /* static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" }; */
 static const char *tags[] = { "", "", "", "", "", "", "", "", "" };
-
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -76,11 +76,11 @@ static const Layout layouts[] = {
 
 /* key definitions */
 #define MODKEY Mod4Mask
-#define TAGKEYS(KEY,TAG) \
-	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
-	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
-	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
-	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
+#define TAGKEYS(CHAIN,KEY,TAG) \
+	{ MODKEY,                       CHAIN,    KEY,      view,           {.ui = 1 << TAG} }, \
+	{ MODKEY|ControlMask,           CHAIN,    KEY,      toggleview,     {.ui = 1 << TAG} }, \
+	{ MODKEY|ShiftMask,             CHAIN,    KEY,      tag,            {.ui = 1 << TAG} }, \
+	{ MODKEY|ControlMask|ShiftMask, CHAIN,    KEY,      toggletag,      {.ui = 1 << TAG} },
 #define CMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
@@ -100,74 +100,80 @@ static const char *termcmd[]     = { "st", NULL };
 static const char *tabtermcmd[]  = { "tabbed", "-r", "2", "st", "-w", "''", NULL };
 
 static Key keys[] = {
-	/* modifier             key        function        argument */
-	{ MODKEY|ShiftMask,     XK_Return, spawn,          {.v = dmenucmd } },
-	{ MODKEY,               XK_Return, spawn,          {.v = termcmd } },
-	{ Mod1Mask,             XK_Return, spawn,          {.v = tabtermcmd } },
-	{ MODKEY,               XK_b,      togglebar,      {0} },
-	{ MODKEY|ShiftMask,     XK_j,      rotatestack,    {.i = +1 } },
-	{ MODKEY|ShiftMask,     XK_k,      rotatestack,    {.i = -1 } },
-	{ MODKEY,               XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,               XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,               XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,               XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,               XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,               XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY|ControlMask,   XK_Return, zoom,           {0} },
-	{ MODKEY,               XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,     XK_c,      killclient,     {0} },
+	/* modifier             chain key  key        function        argument */
+	{ MODKEY|ShiftMask,     -1,        XK_Return, spawn,          {.v = dmenucmd } },
+	{ MODKEY,               -1,        XK_Return, spawn,          {.v = termcmd } },
+	{ Mod1Mask,             -1,        XK_Return, spawn,          {.v = tabtermcmd } },
+	{ MODKEY,               -1,        XK_b,      togglebar,      {0} },
+	{ MODKEY|ShiftMask,     -1,        XK_j,      rotatestack,    {.i = +1 } },
+	{ MODKEY|ShiftMask,     -1,        XK_k,      rotatestack,    {.i = -1 } },
+	{ MODKEY,               -1,        XK_j,      focusstack,     {.i = +1 } },
+	{ MODKEY,               -1,        XK_k,      focusstack,     {.i = -1 } },
+	{ MODKEY,               -1,        XK_i,      incnmaster,     {.i = +1 } },
+	{ MODKEY,               -1,        XK_d,      incnmaster,     {.i = -1 } },
+	{ MODKEY,               -1,        XK_h,      setmfact,       {.f = -0.05} },
+	{ MODKEY,               -1,        XK_l,      setmfact,       {.f = +0.05} },
+	{ MODKEY|ControlMask,   -1,        XK_Return, zoom,           {0} },
+	{ MODKEY,               -1,        XK_Tab,    view,           {0} },
+	{ MODKEY|ShiftMask,     -1,        XK_c,      killclient,     {0} },
 
     /* Layout manipulation */
-	{ MODKEY,               XK_Tab,    cyclelayout,    {.i = -1 } },
-	{ MODKEY|ShiftMask,     XK_Tab,    cyclelayout,    {.i = +1 } },
-	{ MODKEY,               XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,     XK_space,  togglefloating, {0} },
+	{ MODKEY,               -1,        XK_Tab,    cyclelayout,    {.i = -1 } },
+	{ MODKEY|ShiftMask,     -1,        XK_Tab,    cyclelayout,    {.i = +1 } },
+	{ MODKEY,               -1,        XK_space,  setlayout,      {0} },
+	{ MODKEY|ShiftMask,     -1,        XK_space,  togglefloating, {0} },
 
     /* Switch to specific layouts */
-	{ MODKEY,               XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,               XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,               XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,               XK_g,      setlayout,      {.v = &layouts[3]} },
+	{ MODKEY,               -1,        XK_t,      setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,               -1,        XK_f,      setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,               -1,        XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,               -1,        XK_g,      setlayout,      {.v = &layouts[3]} },
 
-	{ MODKEY,               XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,     XK_0,      tag,            {.ui = ~0 } },
+	{ MODKEY,               -1,        XK_0,      view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,     -1,        XK_0,      tag,            {.ui = ~0 } },
 
     /* Switching between monitors */
-	{ MODKEY,               XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,               XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,     XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,     XK_period, tagmon,         {.i = +1 } },
+	{ MODKEY,               -1,        XK_comma,  focusmon,       {.i = -1 } },
+	{ MODKEY,               -1,        XK_period, focusmon,       {.i = +1 } },
+	{ MODKEY|ShiftMask,     -1,        XK_comma,  tagmon,         {.i = -1 } },
+	{ MODKEY|ShiftMask,     -1,        XK_period, tagmon,         {.i = +1 } },
 	
-    /* Apps Launched with SUPER + ALT + KEY */
-	{ MODKEY|Mod1Mask,        XK_b,    spawn,          CMD("tabbed -r 2 surf -pe x '.surf/html/homepage.html'") },
-	{ MODKEY|Mod1Mask,        XK_c,    spawn,          CMD("st -e cmus") },
-	{ MODKEY|Mod1Mask,        XK_e,    spawn,          CMD("st -e emacsclient -c -a emacs") },
-	{ MODKEY|Mod1Mask,        XK_f,    spawn,          CMD("st -e vifm") },
-	{ MODKEY|Mod1Mask,        XK_h,    spawn,          CMD("st -e htop") },
-	{ MODKEY|Mod1Mask,        XK_i,    spawn,          CMD("st -e irssi") },
-	{ MODKEY|Mod1Mask,        XK_l,    spawn,          CMD("st -e lynx gopher://distro.tube") },
-	{ MODKEY|Mod1Mask,        XK_n,    spawn,          CMD("st -e newsboat") },
-	{ MODKEY|Mod1Mask,        XK_r,    spawn,          CMD("st -e rtv") },
+    /* Keybindings for programs using the format SUPER + ALT + "key" */
+	{ MODKEY|Mod1Mask,      -1,        XK_s,      spawn,          CMD("tabbed -r 2 surf -pe x '.surf/html/homepage.html'") },
+	{ MODKEY|Mod1Mask,      -1,        XK_b,      spawn,          CMD("brave") },
+	{ MODKEY|Mod1Mask,      -1,        XK_m,      spawn,          CMD("mailspring") },
+	{ MODKEY|Mod1Mask,      -1,        XK_f,      spawn,          CMD("pcmanfm") },
 	
-    /* Dmenu scripts launched with ALT + CTRL + KEY */
-	{ Mod1Mask|ControlMask, XK_e,      spawn,          CMD("./.dmenu/dmenu-edit-configs.sh") },
-	{ Mod1Mask|ControlMask, XK_m,      spawn,          CMD("./.dmenu/dmenu-sysmon.sh") },
-	{ Mod1Mask|ControlMask, XK_p,      spawn,          CMD("passmenu") },
-	{ Mod1Mask|ControlMask, XK_r,      spawn,          CMD("./.dmenu/dmenu-reddio.sh") },
-	{ Mod1Mask|ControlMask, XK_s,      spawn,          CMD("./.dmenu/dmenu-surfraw.sh") },
-	{ Mod1Mask|ControlMask, XK_i,      spawn,          CMD("./.dmenu/dmenu-scrot.sh") },
+    /* Dmenu scripts launched with emacs-style keychords SUPER + p followed by "key" */
+	{ MODKEY,               XK_p,      XK_e,      spawn,          CMD("./dmscripts/dmconf") },
+	{ MODKEY,               XK_p,      XK_i,      spawn,          CMD("./dmscripts/dmscrot") },
+	{ MODKEY,               XK_p,      XK_k,      spawn,          CMD("./dmscripts/dmkill") },
+	{ MODKEY,               XK_p,      XK_l,      spawn,          CMD("./dmscripts/dmlogout") },
+	{ MODKEY,               XK_p,      XK_m,      spawn,          CMD("./dmscripts/dman") },
+	{ MODKEY,               XK_p,      XK_r,      spawn,          CMD("./dmscripts/dmred") },
+	{ MODKEY,               XK_p,      XK_s,      spawn,          CMD("./dmscripts/dmsearch") },
+	{ MODKEY,               XK_p,      XK_p,      spawn,          CMD("passmenu") },
     
-	TAGKEYS(                  XK_1,          0)
-	TAGKEYS(                  XK_2,          1)
-	TAGKEYS(                  XK_3,          2)
-	TAGKEYS(                  XK_4,          3)
-	TAGKEYS(                  XK_5,          4)
-	TAGKEYS(                  XK_6,          5)
-	TAGKEYS(                  XK_7,          6)
-	TAGKEYS(                  XK_8,          7)
-	TAGKEYS(                  XK_9,          8)
-	{ MODKEY|ShiftMask,       XK_q,	   quit,		   {0} },
-    { MODKEY|ShiftMask,       XK_r,    quit,           {1} }, 
+    /* Doom emacs keybindings use the keychord CTRL + e followed by "key" */
+	{ ControlMask,          XK_e,      XK_e,      spawn,          CMD("emacsclient -c -a 'emacs'") },
+	{ ControlMask,          XK_e,      XK_b,      spawn,          CMD("emacsclient -c -a 'emacs' --eval '(ibuffer)'") },
+	{ ControlMask,          XK_e,      XK_d,      spawn,          CMD("emacsclient -c -a 'emacs' --eval '(dired nil)'") },
+	{ ControlMask,          XK_e,      XK_m,      spawn,          CMD("emacsclient -c -a 'emacs' --eval '(mu4e)'") },
+	{ ControlMask,          XK_e,      XK_n,      spawn,          CMD("emacsclient -c -a 'emacs' --eval '(elfeed)'") },
+	{ ControlMask,          XK_e,      XK_s,      spawn,          CMD("emacsclient -c -a 'emacs' --eval '(eshell)'") },
+	{ ControlMask,          XK_e,      XK_v,      spawn,          CMD("emacsclient -c -a 'emacs' --eval '(+vterm/here nil)'") },
+
+	TAGKEYS(                -1,        XK_1,                      0)
+	TAGKEYS(                -1,        XK_2,                      1)
+	TAGKEYS(                -1,        XK_3,                      2)
+	TAGKEYS(                -1,        XK_4,                      3)
+	TAGKEYS(                -1,        XK_5,                      4)
+	TAGKEYS(                -1,        XK_6,                      5)
+	TAGKEYS(                -1,        XK_7,                      6)
+	TAGKEYS(                -1,        XK_8,                      7)
+	TAGKEYS(                -1,        XK_9,                      8)
+	{ MODKEY|ShiftMask,     -1,        XK_q,	  quit,		      {0} },
+    { MODKEY|ShiftMask,     -1,        XK_r,      quit,           {1} }, 
 };
 
 /* button definitions */
