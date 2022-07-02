6b49796f4c6c80414a7d56d3703e996406bf6a41
Derek Taylor <derek@distrotube.com>
2022-07-02 14:56:26 -0500

    Redoing my dwm to 6.3 version. Adding some new patches.

--- config.def.h
+++ config.def.h
@@ -1,25 +1,24 @@
 /*  ____ _____  */
 /* |  _ \_   _|  Derek Taylor (DistroTube) */
 /* | | | || |  	http://www.youtube.com/c/DistroTube */
-/* | |_| || |  	http://www.gitlab.com/dwt1/ */
-/* |____/ |_|  	*/ 
+/* | |_| || |  	http://www.gitlab.com/dwt1/dwm-distrotube */
+/* |____/ |_|  	*/
 
 /* See LICENSE file for copyright and license details. */
+
 /* appearance */
-static const unsigned int borderpx = 2;   /* border pixel of windows */
-static const unsigned int snap     = 32;  /* snap pixel */
-static const unsigned int gappx    = 6;   /* pixel gap between clients */
-static const int showbar           = 1;   /* 0 means no bar */
-static const int topbar            = 1;   /* 0 means bottom bar */
-static const int horizpadbar       = 6;   /* horizontal padding for statusbar */
-static const int vertpadbar        = 7;   /* vertical padding for statusbar */
-/* Mononoki Nerd Font must be installed from AUR nerd-fonts-complete.
- * Otherwise, your default font will be Hack which is found in the standard
- * Arch repos and is listed as a dependency for this build. JoyPixels is also
- * a hard dependency and makes colored fonts and emojis possible.
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int gappx     = 6;        /* gaps between windows */
+static const unsigned int snap      = 32;       /* snap pixel */
+static const int showbar            = 1;        /* 0 means no bar */
+static const int topbar             = 1;        /* 0 means bottom bar */
+static const int horizpadbar        = 6;        /* horizontal padding for statusbar */
+static const int vertpadbar         = 7;        /* vertical padding for statusbar */
+/* Default font will be Ubuntu if installed (ttf-ubuntu).
+ * Otherwise, your default font will be Hack (ttf-hack)
+ * JoyPixels (ttf-joypixels) is a dependency for colored fonts and emojis.
  */
-static const char *fonts[]     = {"SauceCodePro Nerd Font Mono:weight=bold:size=9:antialias=true:hinting=true",
-	                              "Mononoki:size=9:antialias=true:autohint=true",
+static const char *fonts[]     = {"Ubuntu:weight=bold:size=8:antialias=true:hinting=true",
                                   "Hack:size=8:antialias=true:autohint=true",
                                   "JoyPixels:size=10:antialias=true:autohint=true"
 						     	};
@@ -27,13 +26,13 @@ static const char col_1[]  = "#282c34"; /* background color of bar */
 static const char col_2[]  = "#282c34"; /* border color unfocused windows */
 static const char col_3[]  = "#d7d7d7";
 static const char col_4[]  = "#924441"; /* border color focused windows and tags */
-/* bar opacity 
+/* bar opacity
  * 0xff is no transparency.
  * 0xee adds wee bit of transparency.
  * 0xdd adds adds a bit more transparency.
  * Play with the value to get desired transparency.
  */
-static const unsigned int baralpha    = 0xff; 
+static const unsigned int baralpha    = 0xff;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]        = {
 	/*               fg         bg         border   */
@@ -62,137 +61,131 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact     = 0.50; /* factor of master area size [0.05..0.95] */
-static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
+static const float mfact        = 0.55; /* factor of master area size [0.05..0.95] */
+static const int nmaster        = 1;    /* number of clients in master area */
+static const int resizehints    = 0;    /* 1 means respect size hints in tiled resizals */
+static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
-#include "layouts.c"
+#include "grid.c"
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
 	{ "HHH",      grid },
-	{ NULL,       NULL },
+    { NULL,       NULL },
 };
 
 /* key definitions */
 #define MODKEY Mod4Mask
-#define TAGKEYS(CHAIN,KEY,TAG) \
-	{ MODKEY,                       CHAIN,    KEY,      view,           {.ui = 1 << TAG} }, \
-	{ MODKEY|ControlMask,           CHAIN,    KEY,      toggleview,     {.ui = 1 << TAG} }, \
-	{ MODKEY|ShiftMask,             CHAIN,    KEY,      tag,            {.ui = 1 << TAG} }, \
-	{ MODKEY|ControlMask|ShiftMask, CHAIN,    KEY,      toggletag,      {.ui = 1 << TAG} },
-#define CMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
+
+#define TAGKEYS(KEY,TAG)												\
+	{1, {{MODKEY, KEY}},								view,           {.ui = 1 << TAG} },	\
+	{1, {{MODKEY|ControlMask, KEY}},					toggleview,     {.ui = 1 << TAG} }, \
+	{1, {{MODKEY|ShiftMask, KEY}},						tag,            {.ui = 1 << TAG} }, \
+	{1, {{MODKEY|ControlMask|ShiftMask, KEY}},			toggletag,      {.ui = 1 << TAG} },
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
-/* dmenu */
+/* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-/* If you are using the standard dmenu program, use the following. */
-static const char *dmenucmd[]    = { "dmenu_run", "-p", "Run: ", NULL };
-/* If you are using the dmenu-distrotube-git program, use the following for a cooler dmenu! */
-/* static const char *dmenucmd[]    = { "dmenu_run", "-g", "10", "-l", "48", "-p", "Run: ", NULL }; */
-
-/* the st terminal with tabbed */
-static const char *termcmd[]     = { "st", NULL };
-/* An alternative way to launch st along with the fish shell */
-/* static const char *termcmd[]     = { "st", "-e fish", NULL }; */
+static const char *dmenucmd[]    = { "dm-run", NULL };
+static const char *termcmd[]  = { "st", NULL };
 static const char *tabtermcmd[]  = { "tabbed", "-r", "2", "st", "-w", "''", NULL };
 
-static Key keys[] = {
-	/* modifier             chain key  key        function        argument */
-	{ MODKEY|ShiftMask,     -1,        XK_Return, spawn,          {.v = dmenucmd } },
-	{ MODKEY,               -1,        XK_Return, spawn,          {.v = termcmd } },
-	{ Mod1Mask,             -1,        XK_Return, spawn,          {.v = tabtermcmd } },
-	{ MODKEY,               -1,        XK_b,      togglebar,      {0} },
-	{ MODKEY|ShiftMask,     -1,        XK_j,      rotatestack,    {.i = +1 } },
-	{ MODKEY|ShiftMask,     -1,        XK_k,      rotatestack,    {.i = -1 } },
-	{ MODKEY,               -1,        XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,               -1,        XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY|ShiftMask,     -1,        XK_Up,     incnmaster,     {.i = +1 } },
-	{ MODKEY|ShiftMask,     -1,        XK_Down,   incnmaster,     {.i = -1 } },
-	{ MODKEY,               -1,        XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,               -1,        XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY|ControlMask,   -1,        XK_Return, zoom,           {0} },
-	{ MODKEY,               -1,        XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,     -1,        XK_c,      killclient,     {0} },
+static Keychord keychords[] = {
+	/* Keys        function        argument */
+	{1, {{MODKEY|ShiftMask, XK_Return}}, spawn,          {.v = dmenucmd } },
+	{1, {{MODKEY, XK_Return}},			 spawn,          {.v = termcmd } },
+	{1, {{Mod1Mask, XK_Return}},         spawn,          {.v = tabtermcmd } },
+	{1, {{MODKEY|ShiftMask, XK_c}},		 killclient,     {0} },
+
+	/* Dmenu scripts launched with emacs-style keychords SUPER + p followed by "key" */
+	{2, {{MODKEY, XK_p}, {0, XK_e}},      spawn,         SHCMD("dm-confedit") },
+	{2, {{MODKEY, XK_p}, {0, XK_i}},      spawn,         SHCMD("dm-maim") },
+	{2, {{MODKEY, XK_p}, {0, XK_k}},      spawn,         SHCMD("dm-kill") },
+	{2, {{MODKEY, XK_p}, {0, XK_l}},      spawn,         SHCMD("dm-logout") },
+	{2, {{MODKEY, XK_p}, {0, XK_m}},      spawn,         SHCMD("dm-man") },
+	{2, {{MODKEY, XK_p}, {0, XK_r}},      spawn,         SHCMD("dm-reddit") },
+	{2, {{MODKEY, XK_p}, {0, XK_s}},      spawn,         SHCMD("dm-websearch") },
+	{2, {{MODKEY, XK_p}, {0, XK_p}},      spawn,         SHCMD("passmenu") },
+
+	{2, {{MODKEY, XK_e}, {0, XK_e}},      spawn,         SHCMD("emacsclient -c -a 'emacs'") },
+	{2, {{MODKEY, XK_e}, {0, XK_a}},      spawn,         SHCMD("emacsclient -c -a 'emacs' --eval '(emms)' --eval '(emms-play-directory-tree \"~/Music/\")'") },
+	{2, {{MODKEY, XK_e}, {0, XK_b}},      spawn,         SHCMD("emacsclient -c -a 'emacs' --eval '(ibuffer)'") },
+	{2, {{MODKEY, XK_e}, {0, XK_d}},      spawn,         SHCMD("emacsclient -c -a 'emacs' --eval '(dired nil)'") },
+	{2, {{MODKEY, XK_e}, {0, XK_i}},      spawn,         SHCMD("emacsclient -c -a 'emacs' --eval '(erc)'") },
+	{2, {{MODKEY, XK_e}, {0, XK_n}},      spawn,         SHCMD("emacsclient -c -a 'emacs' --eval '(elfeed)'") },
+	{2, {{MODKEY, XK_e}, {0, XK_s}},      spawn,         SHCMD("emacsclient -c -a 'emacs' --eval '(eshell)'") },
+	{2, {{MODKEY, XK_e}, {0, XK_v}},      spawn,         SHCMD("emacsclient -c -a 'emacs' --eval '(+vterm/here nil)'") },
+	{2, {{MODKEY, XK_e}, {0, XK_w}},      spawn,         SHCMD("emacsclient -c -a 'emacs' --eval '(doom/window-maximize-buffer(eww \"distro.tube\"))'") },
+
+    /* Web browsers */
+    {1, {{MODKEY, XK_b}},                 spawn,         SHCMD("qutebrowser") },
+    {1, {{MODKEY|Mod1Mask, XK_s}},        spawn,         SHCMD("tabbed -r 2 surf -pe x '.surf/html/homepage.html'") },
+
+
+	{1, {{MODKEY, XK_b}},				  togglebar,      {0} },
+	{1, {{MODKEY, XK_j}},				  focusstack,     {.i = +1 } },
+	{1, {{MODKEY, XK_k}},				  focusstack,     {.i = -1 } },
+	{1, {{MODKEY|ShiftMask, XK_j}},		  rotatestack,    {.i = +1 } },
+	{1, {{MODKEY|ShiftMask, XK_k}},		  rotatestack,    {.i = -1 } },
+	{1, {{MODKEY, XK_i}},				  incnmaster,     {.i = +1 } },
+	{1, {{MODKEY, XK_d}},				  incnmaster,     {.i = -1 } },
+	{1, {{MODKEY, XK_h}},				  setmfact,       {.f = -0.05} },
+	{1, {{MODKEY, XK_l}},				  setmfact,       {.f = +0.05} },
+	{1, {{MODKEY, XK_Return}},			  zoom,           {0} },
+	{1, {{MODKEY, XK_Tab}},				  view,           {0} },
 
     /* Layout manipulation */
-	{ MODKEY,               -1,        XK_Tab,    cyclelayout,    {.i = -1 } },
-	{ MODKEY|ShiftMask,     -1,        XK_Tab,    cyclelayout,    {.i = +1 } },
-	{ MODKEY,               -1,        XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,     -1,        XK_space,  togglefloating, {0} },
+	{1, {{MODKEY, XK_Tab}},				  cyclelayout,    {.i = -1 } },
+	{1, {{MODKEY|ShiftMask, XK_Tab}},		  cyclelayout,    {.i = +1 } },
+	{1, {{MODKEY, XK_t}},				  setlayout,      {.v = &layouts[0]} },
+	{1, {{MODKEY, XK_f}},				  setlayout,      {.v = &layouts[1]} },
+	{1, {{MODKEY, XK_m}},				  setlayout,      {.v = &layouts[2]} },
+	{1, {{MODKEY, XK_g}},				  setlayout,      {.v = &layouts[3]} },
+	{1, {{MODKEY, XK_space}},			  setlayout,      {0} },
 
-    /* Switch to specific layouts */
-	{ MODKEY,               -1,        XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,               -1,        XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,               -1,        XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,               -1,        XK_g,      setlayout,      {.v = &layouts[3]} },
-
-	{ MODKEY,               -1,        XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,     -1,        XK_0,      tag,            {.ui = ~0 } },
+	{1, {{MODKEY|ShiftMask, XK_space}},	  togglefloating, {0} },
+	{1, {{MODKEY, XK_0}},				  view,           {.ui = ~0 } },
+	{1, {{MODKEY|ShiftMask, XK_0}},		  tag,            {.ui = ~0 } },
 
     /* Switching between monitors */
-	{ MODKEY,               -1,        XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,               -1,        XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,     -1,        XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,     -1,        XK_period, tagmon,         {.i = +1 } },
-	
-    /* Favorite programs */
-	{ MODKEY,               -1,        XK_b,      spawn,          CMD("qutebrowser") },
-	{ MODKEY|Mod1Mask,      -1,        XK_s,      spawn,          CMD("tabbed -r 2 surf -pe x '.surf/html/homepage.html'") },
+	{1, {{MODKEY, XK_comma}},			  focusmon,       {.i = -1 } },
+	{1, {{MODKEY, XK_period}},			  focusmon,       {.i = +1 } },
+	{1, {{MODKEY|ShiftMask, XK_comma}},	  tagmon,         {.i = -1 } },
+	{1, {{MODKEY|ShiftMask, XK_period}},  tagmon,         {.i = +1 } },
 
-    /* Dmenu scripts launched with emacs-style keychords SUPER + p followed by "key" */
-	{ MODKEY,               XK_p,      XK_e,      spawn,          CMD("./dmscripts/dm-confedit") },
-	{ MODKEY,               XK_p,      XK_i,      spawn,          CMD("./dmscripts/dm-maim") },
-	{ MODKEY,               XK_p,      XK_k,      spawn,          CMD("./dmscripts/dm-kill") },
-	{ MODKEY,               XK_p,      XK_l,      spawn,          CMD("./dmscripts/dm-logout") },
-	{ MODKEY,               XK_p,      XK_m,      spawn,          CMD("./dmscripts/dm-man") },
-	{ MODKEY,               XK_p,      XK_r,      spawn,          CMD("./dmscripts/dm-reddit") },
-	{ MODKEY,               XK_p,      XK_s,      spawn,          CMD("./dmscripts/dm-websearch") },
-	{ MODKEY,               XK_p,      XK_p,      spawn,          CMD("passmenu") },
-    
-    /* Doom emacs keybindings use the keychord SUPER + e followed by "key" */
-	{ MODKEY,               XK_e,      XK_e,      spawn,          CMD("emacsclient -c -a 'emacs'") },
-	{ MODKEY,               XK_e,      XK_a,      spawn,          CMD("emacsclient -c -a 'emacs' --eval '(emms)' --eval '(emms-play-directory-tree \"~/Music/\")'") },
-	{ MODKEY,               XK_e,      XK_b,      spawn,          CMD("emacsclient -c -a 'emacs' --eval '(ibuffer)'") },
-	{ MODKEY,               XK_e,      XK_d,      spawn,          CMD("emacsclient -c -a 'emacs' --eval '(dired nil)'") },
-	{ MODKEY,               XK_e,      XK_i,      spawn,          CMD("emacsclient -c -a 'emacs' --eval '(erc)'") },
-	{ MODKEY,               XK_e,      XK_n,      spawn,          CMD("emacsclient -c -a 'emacs' --eval '(elfeed)'") },
-	{ MODKEY,               XK_e,      XK_s,      spawn,          CMD("emacsclient -c -a 'emacs' --eval '(eshell)'") },
-	{ MODKEY,               XK_e,      XK_v,      spawn,          CMD("emacsclient -c -a 'emacs' --eval '(+vterm/here nil)'") },
-	{ MODKEY,               XK_e,      XK_w,      spawn,          CMD("emacsclient -c -a 'emacs' --eval '(doom/window-maximize-buffer(eww \"distro.tube\"))'") },
+	TAGKEYS(                        XK_1,                      0)
+	TAGKEYS(                        XK_2,                      1)
+	TAGKEYS(                        XK_3,                      2)
+	TAGKEYS(                        XK_4,                      3)
+	TAGKEYS(                        XK_5,                      4)
+	TAGKEYS(                        XK_6,                      5)
+	TAGKEYS(                        XK_7,                      6)
+	TAGKEYS(                        XK_8,                      7)
+	TAGKEYS(                        XK_9,                      8)
 
-	TAGKEYS(                -1,        XK_1,                      0)
-	TAGKEYS(                -1,        XK_2,                      1)
-	TAGKEYS(                -1,        XK_3,                      2)
-	TAGKEYS(                -1,        XK_4,                      3)
-	TAGKEYS(                -1,        XK_5,                      4)
-	TAGKEYS(                -1,        XK_6,                      5)
-	TAGKEYS(                -1,        XK_7,                      6)
-	TAGKEYS(                -1,        XK_8,                      7)
-	TAGKEYS(                -1,        XK_9,                      8)
-	{ MODKEY|ShiftMask,     -1,        XK_q,	  quit,		      {0} },
-    { MODKEY|ShiftMask,     -1,        XK_r,      quit,           {1} }, 
+    /* Restart or quit dwm */
+	{1, {{MODKEY|ShiftMask, XK_r}},		  quit,           {1} },
+	{1, {{MODKEY|ShiftMask, XK_q}},		  quit,           {0} },
 };
 
 /* button definitions */
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static Button buttons[] = {
-	/* click           event mask   button          function        argument */
-	{ ClkLtSymbol,     0,           Button1,        setlayout,      {0} },
-	{ ClkLtSymbol,     0,           Button3,        setlayout,      {.v = &layouts[2]} },
-	{ ClkWinTitle,     0,           Button2,        zoom,           {0} },
-	{ ClkStatusText,   0,           Button2,        spawn,          {.v = termcmd } },
-	{ ClkClientWin,    MODKEY,      Button1,        movemouse,      {0} },
-	{ ClkClientWin,    MODKEY,      Button2,        togglefloating, {0} },
-	{ ClkClientWin,    MODKEY,      Button3,        resizemouse,    {0} },
-	{ ClkTagBar,       0,           Button1,        view,           {0} },
-	{ ClkTagBar,       0,           Button3,        toggleview,     {0} },
-	{ ClkTagBar,       MODKEY,      Button1,        tag,            {0} },
-	{ ClkTagBar,       MODKEY,      Button3,        toggletag,      {0} },
+	/* click                event mask      button          function        argument */
+	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
+	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
+	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
+	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
+	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
+	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
+	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
+	{ ClkTagBar,            0,              Button1,        view,           {0} },
+	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
+	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
+	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
-
 
