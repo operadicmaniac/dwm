94cabf0083ea8aade270019537d95731b957e695
Manmohan Shukla <83252585+operadicmaniac@users.noreply.github.com>
2022-09-12 20:16:40 +0530

    Update config.def.h

--- config.def.h
+++ config.def.h
@@ -1,14 +1,7 @@
-/*  ____ _____  */
-/* |  _ \_   _|  Derek Taylor (DistroTube) */
-/* | | | || |  	http://www.youtube.com/c/DistroTube */
-/* | |_| || |  	http://www.gitlab.com/dwt1/dwm-distrotube */
-/* |____/ |_|  	*/
-
-/* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int gappx     = 6;        /* gaps between windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const unsigned int gappx     = 4;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -22,10 +15,10 @@ static const char *fonts[]     = {"Ubuntu:weight=bold:size=8:antialias=true:hint
                                   "Hack:size=8:antialias=true:autohint=true",
                                   "JoyPixels:size=10:antialias=true:autohint=true"
 						     	};
-static const char col_1[]  = "#282c34"; /* background color of bar */
+static const char col_1[]  = "#2d313b"; /* background color of bar */
 static const char col_2[]  = "#282c34"; /* border color unfocused windows */
 static const char col_3[]  = "#d7d7d7";
-static const char col_4[]  = "#924441"; /* border color focused windows and tags */
+static const char col_4[]  = "#018f67"; /* border color focused windows and tags */
 /* bar opacity
  * 0xff is no transparency.
  * 0xee adds wee bit of transparency.
@@ -48,7 +41,7 @@ static const unsigned int alphas[][3] = {
 /* tagging */
 /* static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" }; */
 /* static const char *tags[] = { "", "", "", "", "", "", "", "", "" }; */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "8" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -91,8 +84,10 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[]    = { "dmenu_run", NULL };
-static const char *termcmd[]  = { "alacritty", NULL };
+static const char *termcmd[]  = { "st", NULL };
 static const char *tabtermcmd[]  = { "tabbed", "-r", "2", "st", "-w", "''", NULL };
+
+
 
 static Keychord keychords[] = {
 	/* Keys        function        argument */
@@ -101,6 +96,7 @@ static Keychord keychords[] = {
 	{1, {{Mod1Mask, XK_Return}},         spawn,          {.v = tabtermcmd } },
 	{1, {{MODKEY|ShiftMask, XK_c}},		 killclient,     {0} },
 
+	
 	/* Dmenu scripts launched with emacs-style keychords SUPER + p followed by "key" */
 	{2, {{MODKEY, XK_p}, {0, XK_e}},      spawn,         SHCMD("dm-confedit") },
 	{2, {{MODKEY, XK_p}, {0, XK_i}},      spawn,         SHCMD("dm-maim") },
@@ -122,7 +118,7 @@ static Keychord keychords[] = {
 	{2, {{MODKEY, XK_e}, {0, XK_w}},      spawn,         SHCMD("emacsclient -c -a 'emacs' --eval '(doom/window-maximize-buffer(eww \"distro.tube\"))'") },
 
     /* Web browsers */
-    {1, {{MODKEY, XK_b}},                 spawn,         SHCMD("qutebrowser") },
+    {1, {{MODKEY|ShiftMask, XK_b}},                 spawn,         SHCMD("firefox") },
     {1, {{MODKEY|Mod1Mask, XK_s}},        spawn,         SHCMD("tabbed -r 2 surf -pe x '.surf/html/homepage.html'") },
 
 
@@ -169,7 +165,7 @@ static Keychord keychords[] = {
 
     /* Restart or quit dwm */
 	{1, {{MODKEY|ShiftMask, XK_r}},		  quit,           {1} },
-	{1, {{MODKEY|ShiftMask, XK_q}},		  quit,           {0} },
+	{1, {{MODKEY|ShiftMask, XK_q}},		  quit,           {1} },
 };
 
 /* button definitions */
@@ -188,4 +184,3 @@ static Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
-
